54e2e76b2ab7ad8428c5a540f3e4925a949e6482
Nathan <nathanfalima189@gmail.com>
2026-04-06 21:35:24 -0300

    1.4 Faça um programa que gere três valores reais a,b,c e mostre o valor de delta: aquele que calculamos para chegar ás potenciais raízes de uma equação de segundo grau.

--- Apostila08_NathanLima_VictorBlanco.sql
+++ Apostila08_NathanLima_VictorBlanco.sql
@@ -22,3 +22,20 @@ BEGIN
 	RAISE NOTICE 'O valor gerado é: % resultado em Fahrenheit: %', ROUND(valor,2), ROUND( (valor * 1.8) + 32, 2 ) ; 
 END; 
 $$
+
+
+-- 1.4 Faça um programa que gere três valores reais a, b, e c e mostre o valor de delta: aquele que calculamos para chegar às potenciais raízes de uma equação do segundo grau.
+DO $$ 
+DECLARE
+    a NUMERIC := RANDOM() * 10;
+    b NUMERIC := RANDOM() * 10;
+    c NUMERIC := RANDOM() * 10;
+    delta NUMERIC;
+    x1 NUMERIC;
+    x2 NUMERIC;
+BEGIN 
+	delta :=  (b ^ 2) - (4 * a * c);
+  
+    RAISE NOTICE 'DELTA %', delta;
+END; 
+$$
