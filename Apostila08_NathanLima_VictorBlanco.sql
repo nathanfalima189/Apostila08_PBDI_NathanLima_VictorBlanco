24c21cbbfbef8c67264a37a47038d786e0e1d946
Nathan <nathanfalima189@gmail.com>
2026-04-06 21:33:26 -0300

    1.3 Faça um programa que gere um valor real entre 20 e 30, que represente um valor em celcius e faça a conversão em fahrenheit

--- Apostila08_NathanLima_VictorBlanco.sql
+++ Apostila08_NathanLima_VictorBlanco.sql
@@ -12,3 +12,13 @@ BEGIN
 	RAISE NOTICE 'O valor gerado é: %', ROUND(valor,2); 
 END; 
 $$
+
+
+-- 1.3 Faça um programa que gere um valor real entre 20 e 30, que represente um valor em celcius e faça a conversao em fahrenheit
+DO $$ 
+DECLARE
+    valor NUMERIC := RANDOM() * (30 - 20 + 1) + 20;
+BEGIN 
+	RAISE NOTICE 'O valor gerado é: % resultado em Fahrenheit: %', ROUND(valor,2), ROUND( (valor * 1.8) + 32, 2 ) ; 
+END; 
+$$
