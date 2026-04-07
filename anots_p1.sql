b63459c23cdc54ec58b7a947a6a3d49ce49fe9da
Victor Blanco <55599144+victorblanco03@users.noreply.github.com>
2026-04-07 10:05:16 -0300

    aqui

---
+++ anots_p1.sql
@@ -0,0 +1,101 @@
+-- Enunciado 2 - Sobrevivência em função da classe social 
+-- Escreva um cursor não vinculado que mostra o número de passageiros sobreviventes que 
+-- viajavam na primeira classe (Pclass = 1). 
+-- Mensagem de commit: feat(p1): encontra sobreviventes da primeira classe
+
+DO $$
+DECLARE
+    cur_sobreviventes REFCURSOR;
+    v_Survived INT;
+    v_tot_sobreviventes INT := 0;
+BEGIN
+    OPEN cur_sobreviventes FOR EXECUTE
+    format
+    (
+        '
+        SELECT survived
+         FROM tb_titanic
+         WHERE pclass = 1
+           AND survived = 1;
+           '
+    );
+    LOOP
+        FETCH cur_sobreviventes INTO v_survived;
+        EXIT WHEN NOT FOUND;
+        v_tot_sobreviventes := v_tot_sobreviventes + 1;
+    END LOOP;
+    CLOSE cur_sobreviventes;
+    RAISE NOTICE 
+        'Número de passageiros sobreviventes na 1ª classe: %', 
+        v_tot_sobreviventes;
+END;
+$$;
+
+-- Enunciado 3 -  Sobrevivência em função do gênero 
+-- Escreva um cursor com query dinâmica que mostra o número de passageiros 
+-- sobreviventes dentre as mulheres (Sex = 'female'). Escreva um condicional para que, se 
+-- não existir nenhuma, o valor -1 seja exibido. 
+-- Mensagem de commit: feat(p1): encontra sobreviventes do sexo feminino 
+
+DO $$
+DECLARE
+    ref refcursor;
+    v_survived INTEGER;
+    v_total INTEGER := 0;
+BEGIN
+    OPEN ref FOR EXECUTE
+    format
+    (
+        '
+        SELECT survived
+         FROM tb_titanic
+         WHERE sex = ''female''
+           AND survived = 1
+           '
+    );
+    LOOP
+        FETCH ref INTO v_survived;
+        EXIT WHEN NOT FOUND;
+
+        v_total := v_total + 1;
+    END LOOP;
+    CLOSE ref;
+    IF v_total = 0 THEN
+        RAISE NOTICE 'Resultado: %', -1;
+    ELSE
+        RAISE NOTICE 'Quantidade de mulheres sobreviventes: %', v_total;
+    END IF;
+END $$;
+
+-- Enunciado 4 -  Tarifa versus embarque 
+-- Dentre os passageiros que pagaram tarifa (Fare) maior que 50, quantos embarcaram em 
+-- Cherbourg (Embarked = 'C')? Escreva um cursor vinculado que exiba esse valor. 
+-- Mensagem de commit: feat(p1): encontra passageiros de Cherbourg com tarifa alta
+
+DO $$
+DECLARE
+    -- Cursor vinculado (explícito)
+    CURSOR cur_cherbourg_fare_alta IS
+        SELECT passengerid
+        FROM tb_titanic
+        WHERE fare > 50
+          AND embarked = 'C';
+
+    v_passengerid INTEGER;
+    v_total INTEGER := 0;
+BEGIN
+    OPEN cur_cherbourg_fare_alta;
+
+    LOOP
+        FETCH cur_cherbourg_fare_alta INTO v_passengerid;
+        EXIT WHEN NOT FOUND;
+
+        v_total := v_total + 1;
+    END LOOP;
+
+    CLOSE cur_cherbourg_fare_alta;
+
+    RAISE NOTICE
+        'Quantidade de passageiros embarcados em Cherbourg com tarifa > 50: %',
+        v_total;
+END $$;
